447384ef08bab9bf3213d024816fcd987b83252a
Usanov-Kirill <kirill.a.usanov@gmail.com>
2026-01-21 19:46:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
 import requests
 
-
 def get_sov():
     r = requests.get("https://api.adviceslip.com/advice", timeout=5)
     r.raise_for_status()
     data = r.json()
     print(data["slip"]["advice"])
 
-def get_soviets():
-    with Session() as s:
-        advices = []
-        for _ in range(3):
-            r = s.get("https://api.adviceslip.com/advice", timeout=5)
-            r.raise_for_status()
-            advices.append(r.json()["slip"]["advice"]["status_code"])
-        print("\n".join(advices))
 
 if __name__ == "__main__":
-    #1
+    # 1
     get_sov()
-
-    #2
-    get_soviets()
-
