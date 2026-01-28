61d08c0208e4f6ba6d48e8e295ef8d5cd5baee86
Usanov-Kirill <kirill.a.usanov@gmail.com>
2026-01-28 19:31:19 +0300

    update

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-import requests
+from fastapi import FastAPI, HTTPException 
+from myproject.utils import json_to_dict_list 
+import os 
+from pathlib import Path 
 
-def get_sov():
-    r = requests.get("https://api.adviceslip.com/advice", timeout=5)
-    r.raise_for_status()
-    data = r.json()
-    print(data["slip"]["advice"])
+DATA = Path(__file__).resolve().parents[2] / "data" / "students.json" 
 
+app = FastAPI() 
 
-if __name__ == "__main__":
-    # 1
-    get_sov()
+@app.get("/") 
+def home_page(): 
+    return {"message": "Привет, Мир!"} 
+
+@app.get("/students") 
+def get_all_students(): 
+    try: 
+        return json_to_dict_list(DATA) 
+    
+    except FileNotFoundError: 
+        raise HTTPException(500, "students.json not found") 
--- utils.py
+++ utils.py
@@ -1,2 +1,9 @@
-def greet(x):
-    return "Hello," + x
+import json 
+
+def dict_list_to_json(dict_list, filename):
+    json_str = json.dumps(dict_list, ensure_ascii=False)
+    return json_str
+
+def json_to_dict_list(filename): 
+    data = json.loads(filename)
+    return data
