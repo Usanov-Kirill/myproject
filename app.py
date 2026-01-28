5b039dfbdfce6c255cd87fd4f5cea448edd660e4
Usanov-Kirill <kirill.a.usanov@gmail.com>
2026-01-28 20:57:11 +0300

    commit

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from fastapi import FastAPI, HTTPException 
-from myproject.utils import json_to_dict_list 
-import os 
-from pathlib import Path 
+from fastapi import FastAPI, HTTPException
+from myproject.utils import json_to_dict_list
+import os
+from pathlib import Path
 
-DATA = Path(__file__).resolve().parents[2] / "data" / "students.json" 
+app = FastAPI()
 
-app = FastAPI() 
+DATA = Path(__file__).resolve().parents[1] /"data"/"students.json"
 
-@app.get("/") 
-def home_page(): 
-    return {"message": "Привет, Мир!"} 
 
-@app.get("/students") 
-def get_all_students(): 
-    try: 
-        return json_to_dict_list(DATA) 
-    
-    except FileNotFoundError: 
-        raise HTTPException(500, "students.json not found") 
+@app.get("/")
+def home_page():
+    return {"message": "Привет, Мир!"}
+
+
+@app.get("/students")
+def get_all_students():
+    try:
+        return json_to_dict_list(DATA)
+
+    except FileNotFoundError:
+        raise HTTPException(500, "students.json not found")
+
+
+@app.get("/students/{grade}")
+def get_stud_grade(grade: int):
+    students = json_to_dict_list(DATA)
+    if grade is not None:
+        arr = []
+        for i in students:
+            if i["grade"] == grade:
+                arr.append(i)
+        return arr
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
-import json 
+import json
+
 
 def dict_list_to_json(dict_list, filename):
-    json_str = json.dumps(dict_list, ensure_ascii=False)
-    return json_str
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(dict_list, f, ensure_ascii=False, indent=2)
 
-def json_to_dict_list(filename): 
-    data = json.loads(filename)
-    return data
+
+def json_to_dict_list(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
